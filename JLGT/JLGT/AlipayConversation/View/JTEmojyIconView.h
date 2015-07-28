4434e777c3e6902c0ce9e2322bd1d6889aa4a513
lpy <814168332@qq.com>
2015-07-28 10:18:09 +0800

    修改pch
    pch文件修改

---
+++ JLGT/JLGT/AlipayConversation/View/JTEmojyIconView.h
@@ -0,0 +1,13 @@
+//
+//  JTEmojyIcon.h
+//  JLGT
+//
+//  Created by 刘蒲艳 on 15/7/28.
+//  Copyright (c) 2015年 ds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JTEmojyIcon : UIView
+
+@end
